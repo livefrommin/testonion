efb32898c8252b50a8f0b046c56e307e524f6a09
Parrot user <user@localhost.localdomain>
2020-11-16 09:47:09 +0000

    optimization

--- java/org/example/ConfProperties.java
+++ java/org/example/ConfProperties.java
@@ -7,21 +7,16 @@ public class ConfProperties {
     protected static Properties PROPERTIES;
     static {
         try {
-            //указание пути до файла с настройками
             fileInputStream = new FileInputStream("src/test/resources/conf.properties");
             PROPERTIES = new Properties();
             PROPERTIES.load(fileInputStream);
         } catch (IOException e) {
             e.printStackTrace();
-            //обработка возможного исключения (нет файла и т.п.)
         } finally {
             if (fileInputStream != null)
                 try {
                     fileInputStream.close();
                 } catch (IOException e) {
                     e.printStackTrace(); } } }
-    /**
-     * метод для возврата строки со значением из файла с настройками
-     */
     public static String getProperty(String key) {
         return PROPERTIES.getProperty(key); } }
